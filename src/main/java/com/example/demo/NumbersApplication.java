464180d8c046aecacfea4fd1a9339ba308aa59d9
DEKaloyanov18 <DEKaloyanov18@codingburgas.bg>
2023-01-23 13:35:06 +0200

    Initial commit

---
+++ src/main/java/com/example/demo/NumbersApplication.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import com.example.demo.AppConfig;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+
+@Import({AppConfig.class})
+
+
+@SpringBootApplication
+public class NumbersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NumbersApplication.class, args);
+    }
+
+}
